d0c6f0025278c496f9b49dd374b3b5ff357b0dcd
Matt Smith <matthew.smith@thinkinglogic.com>
2019-03-12 14:06:41 +0000

    HTBHF-686 Separated api, db and manifest sub-projects

--- api/src/test/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityControllerIntegrationTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityControllerIntegrationTest.java
@@ -13,7 +13,6 @@ import java.net.URI;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.OK;
 
 @ExtendWith(SpringExtension.class)
