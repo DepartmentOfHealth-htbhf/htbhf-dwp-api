cb2cb60ada65f3d9b2c3a5474ff4b7b64d9e4332
Mike Smith <mike.smith@sescon.co.uk>
2019-03-14 15:22:04 +0000

    HTBHF-632 Swagger annotations to generate swagger file.

--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityController.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityController.java
@@ -1,5 +1,10 @@
 package uk.gov.dhsc.htbhf.dwp.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +21,19 @@ import javax.validation.Valid;
 @RequestMapping("v1/dwp/eligibility")
 @AllArgsConstructor
 @Slf4j
+@Api(description = "Endpoints for dealing with DWP Eligibility requests.")
 public class DWPEligibilityController {
 
     private final EligibilityService eligibilityService;
 
     @PostMapping
-    public EligibilityResponse getBenefits(@RequestBody @Valid EligibilityRequest eligibilityRequest) {
+    @ApiOperation("Retrieve the eligibility of a person for Universal Credit")
+    @ApiResponses({@ApiResponse(code = 200, message = "The person's eligibility for Universal Credit", response = EligibilityResponse.class)})
+    public EligibilityResponse getBenefits(
+            @RequestBody
+            @Valid
+            @ApiParam("The eligibility request for Universal Credit")
+            EligibilityRequest eligibilityRequest) {
         log.debug("Received eligibility request");
         return eligibilityService.checkEligibility(eligibilityRequest);
     }
--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/model/EligibilityResponse.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/model/EligibilityResponse.java
@@ -2,6 +2,8 @@ package uk.gov.dhsc.htbhf.dwp.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,17 +11,22 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor(onConstructor_ = {@JsonCreator})
+@ApiModel(description = "The eligibility response from DWP")
 public class EligibilityResponse {
 
     @JsonProperty("eligibilityStatus")
+    @ApiModelProperty(notes = "The eligibility status", example = "ELIGIBLE")
     private EligibilityStatus eligibilityStatus;
 
     @JsonProperty("numberOfChildrenUnderOne")
+    @ApiModelProperty(notes = "The number of children under 1 that the person has", example = "1")
     private final Integer numberOfChildrenUnderOne;
 
     @JsonProperty("numberOfChildrenUnderFour")
+    @ApiModelProperty(notes = "The number of children under 4 that the person has", example = "1")
     private final Integer numberOfChildrenUnderFour;
 
     @JsonProperty("householdIdentifier")
+    @ApiModelProperty(notes = "The identifier for their household", example = "9d6049ee-d6e6-4d9b-ae3d-3b1bdf98682f")
     private final String householdIdentifier;
 }
