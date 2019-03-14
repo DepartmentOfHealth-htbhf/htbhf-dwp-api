91d4e4066707ae4b296568ab956e92fdbb7e4dfb
Mike Smith <mike.smith@sescon.co.uk>
2019-03-14 09:02:18 +0000

    HTBHF-632 Add request id and session id from requests into MDC for logging.

--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/DWPApplication.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/DWPApplication.java
@@ -3,7 +3,11 @@ package uk.gov.dhsc.htbhf.dwp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.dhsc.htbhf.dwp.requestcontext.RequestContext;
 
 @SpringBootApplication
 public class DWPApplication {
@@ -15,5 +19,11 @@ public class DWPApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public RequestContext requestContext() {
+        return new RequestContext();
     }
 }
--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityController.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/controller/DWPEligibilityController.java
@@ -1,6 +1,7 @@
 package uk.gov.dhsc.htbhf.dwp.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,14 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("v1/dwp/eligibility")
 @AllArgsConstructor
+@Slf4j
 public class DWPEligibilityController {
 
     private final EligibilityService eligibilityService;
 
     @PostMapping
     public EligibilityResponse getBenefits(@RequestBody @Valid EligibilityRequest eligibilityRequest) {
+        log.debug("Received eligibility request");
         return eligibilityService.checkEligibility(eligibilityRequest);
     }
 }
