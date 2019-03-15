ee616b76284c415bc6ff5b6d8277ef18f9a77a60
Matt Smith <matthew.smith@thinkinglogic.com>
2019-03-15 15:18:03 +0000

    HTBHF-692 Added tables for uc and legacy benefits

--- db/src/main/resources/db/migration/V1_002__UC_datafeed_initial_format.sql
+++ db/src/main/resources/db/migration/V1_002__UC_datafeed_initial_format.sql
@@ -13,25 +13,26 @@ CREATE TABLE dwp_uc_household (
 
 CREATE TABLE dwp_uc_adult (
     id uuid not null primary key,
-    household_id uuid not null,
+    dwp_uc_household_id uuid not null,
     nino varchar(8),
     forename varchar(500),
     surname varchar(500),
     address_line_1 varchar(500),
     address_line_2 varchar(500),
     address_town_or_city varchar(500),
-    address_postcode varchar(500)
+    address_postcode varchar(10)
 );
 
-ALTER TABLE dwp_uc_adult ADD FOREIGN KEY(household_id) REFERENCES dwp_uc_household (id) ON DELETE CASCADE;
+ALTER TABLE dwp_uc_adult ADD FOREIGN KEY(dwp_uc_household_id) REFERENCES dwp_uc_household (id) ON DELETE CASCADE;
+CREATE INDEX dwp_uc_adult_nino_idx ON dwp_uc_adult (nino);
 
 CREATE TABLE dwp_uc_child (
     id uuid not null primary key,
-    household_uuid uuid not null,
+    dwp_uc_household_id uuid not null,
     forename varchar(500),
     surname varchar(500),
     date_of_birth date,
     effective_date date
 );
 
-ALTER TABLE dwp_uc_child ADD FOREIGN KEY(household_id) REFERENCES dwp_uc_household (id) ON DELETE CASCADE;
+ALTER TABLE dwp_uc_child ADD FOREIGN KEY(dwp_uc_household_id) REFERENCES dwp_uc_household (id) ON DELETE CASCADE;
