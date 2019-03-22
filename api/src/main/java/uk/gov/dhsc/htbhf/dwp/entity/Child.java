fe5bd1cd2299d55b30e218d66066481806ec3326
Gethyn Jones <gethyn1@gmail.com>
2019-03-22 11:08:49 +0000

    HTBHF-694 create and extend entities from base entity

--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/entity/Child.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/entity/Child.java
@@ -6,28 +6,16 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
-import java.util.UUID;
-import javax.persistence.Access;
-import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @SuperBuilder
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
-public abstract class Child {
-
-    @Id
-    @Access(AccessType.PROPERTY)
-    @EqualsAndHashCode.Include
-    private UUID id;
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+public abstract class Child extends BaseEntity {
 
     @Size(max = 500)
     @Column(name = "forename")
@@ -40,16 +28,4 @@ public abstract class Child {
     @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
 
-    /**
-     * Adding a custom getter for the id so that we can compare an entity before and after its initial
-     * persistence and they will be the same.
-     *
-     * @return The id for the entity.
-     */
-    public UUID getId() {
-        if (id == null) {
-            this.id = UUID.randomUUID();
-        }
-        return this.id;
-    }
 }
--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/entity/legacy/LegacyAdult.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/entity/legacy/LegacyAdult.java
@@ -1,23 +1,9 @@
 package uk.gov.dhsc.htbhf.dwp.entity.legacy;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import uk.gov.dhsc.htbhf.dwp.entity.BaseEntity;
 
-import java.util.UUID;
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -26,14 +12,8 @@ import javax.validation.constraints.Size;
 @Table(name = "dwp_legacy_adult")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class LegacyAdult {
-
-    @Id
-    @Getter(AccessLevel.NONE)
-    @Access(AccessType.PROPERTY)
-    @EqualsAndHashCode.Include
-    private UUID id;
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+public class LegacyAdult extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dwp_legacy_household_id", nullable = false)
@@ -50,19 +30,5 @@ public class LegacyAdult {
     @Size(max = 500)
     @Column(name = "surname")
     private String surname;
-
-
-    /**
-     * Adding a custom getter for the id so that we can compare an entity before and after its initial
-     * persistence and they will be the same.
-     *
-     * @return The id for the entity.
-     */
-    public UUID getId() {
-        if (id == null) {
-            this.id = UUID.randomUUID();
-        }
-        return this.id;
-    }
 
 }
