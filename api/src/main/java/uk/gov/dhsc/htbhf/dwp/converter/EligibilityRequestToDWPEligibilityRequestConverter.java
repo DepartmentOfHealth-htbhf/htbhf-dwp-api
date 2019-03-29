3f7f1961f4f7c937e38709b2095c9db177ca3acb
David Wybourn <david.wybourn@gmail.com>
2019-03-29 11:38:36 +0000

    HTBHF-694 Removed person converter and moved into request converter.

--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/converter/EligibilityRequestToDWPEligibilityRequestConverter.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/converter/EligibilityRequestToDWPEligibilityRequestConverter.java
@@ -5,20 +5,28 @@ import org.springframework.stereotype.Component;
 import uk.gov.dhsc.htbhf.dwp.model.DWPEligibilityRequest;
 import uk.gov.dhsc.htbhf.dwp.model.DWPPersonDTO;
 import uk.gov.dhsc.htbhf.dwp.model.EligibilityRequest;
+import uk.gov.dhsc.htbhf.dwp.model.PersonDTO;
 
 @Component
 @AllArgsConstructor
 public class EligibilityRequestToDWPEligibilityRequestConverter {
 
-    private final PersonDTOToDWPPersonConverter converter;
-
     public DWPEligibilityRequest convert(EligibilityRequest eligibilityRequest) {
-        DWPPersonDTO person = converter.convert(eligibilityRequest.getPerson());
         return DWPEligibilityRequest.builder()
-                .person(person)
+                .person(convertPerson(eligibilityRequest.getPerson()))
                 .eligibleStartDate(eligibilityRequest.getEligibleStartDate())
                 .eligibleEndDate(eligibilityRequest.getEligibleEndDate())
                 .ucMonthlyIncomeThreshold(eligibilityRequest.getUcMonthlyIncomeThreshold())
+                .build();
+    }
+
+    public DWPPersonDTO convertPerson(PersonDTO person) {
+        return DWPPersonDTO.builder()
+                .address(person.getAddress())
+                .dateOfBirth(person.getDateOfBirth())
+                .forename(person.getFirstName())
+                .surname(person.getLastName())
+                .nino(person.getNino())
                 .build();
     }
 }
--- api/src/test/java/uk/gov/dhsc/htbhf/dwp/converter/EligibilityRequestToDWPEligibilityRequestTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/dwp/converter/EligibilityRequestToDWPEligibilityRequestTest.java
@@ -1,41 +1,29 @@
 package uk.gov.dhsc.htbhf.dwp.converter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.dhsc.htbhf.dwp.model.DWPEligibilityRequest;
-import uk.gov.dhsc.htbhf.dwp.model.DWPPersonDTO;
 import uk.gov.dhsc.htbhf.dwp.model.EligibilityRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static uk.gov.dhsc.htbhf.dwp.factory.DWPPersonDTOTestDataFactory.aValidDWPPerson;
 import static uk.gov.dhsc.htbhf.dwp.helper.EligibilityRequestTestFactory.anEligibilityRequest;
 
-@SpringBootTest
 public class EligibilityRequestToDWPEligibilityRequestTest {
 
-    @MockBean
-    private PersonDTOToDWPPersonConverter personConverter;
-
-    @Autowired
-    private EligibilityRequestToDWPEligibilityRequestConverter converter;
+    private EligibilityRequestToDWPEligibilityRequestConverter converter = new EligibilityRequestToDWPEligibilityRequestConverter();
 
     @Test
     void shouldConvertRequest() {
         EligibilityRequest request = anEligibilityRequest();
-        DWPPersonDTO person = aValidDWPPerson();
-        given(personConverter.convert(any())).willReturn(person);
 
         DWPEligibilityRequest result = converter.convert(request);
 
         assertThat(result.getEligibleStartDate()).isEqualTo(request.getEligibleStartDate());
         assertThat(result.getEligibleEndDate()).isEqualTo(request.getEligibleEndDate());
         assertThat(result.getUcMonthlyIncomeThreshold()).isEqualTo(request.getUcMonthlyIncomeThreshold());
-        assertThat(result.getPerson()).isEqualTo(person);
-        verify(personConverter).convert(request.getPerson());
+        assertThat(result.getPerson().getAddress()).isEqualTo(request.getPerson().getAddress());
+        assertThat(result.getPerson().getDateOfBirth()).isEqualTo(request.getPerson().getDateOfBirth());
+        assertThat(result.getPerson().getForename()).isEqualTo(request.getPerson().getFirstName());
+        assertThat(result.getPerson().getSurname()).isEqualTo(request.getPerson().getLastName());
+        assertThat(result.getPerson().getNino()).isEqualTo(request.getPerson().getNino());
     }
 }
