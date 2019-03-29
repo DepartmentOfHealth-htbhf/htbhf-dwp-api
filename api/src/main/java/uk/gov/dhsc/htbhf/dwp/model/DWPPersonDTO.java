7ecd0ea0404744c36c64a7f9f232f31ee71d0430
David Wybourn <david.wybourn@gmail.com>
2019-03-29 11:27:26 +0000

    HTBHF-694 Removed validation annotations on objects we create ourselves.

--- api/src/main/java/uk/gov/dhsc/htbhf/dwp/model/DWPPersonDTO.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/dwp/model/DWPPersonDTO.java
@@ -7,35 +7,24 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 
 @Data
 @Builder
 @AllArgsConstructor(onConstructor_ = {@JsonCreator})
 public class DWPPersonDTO {
-    @NotNull
+
     @JsonProperty("forename")
     private final String forename;
 
-    @NotNull
     @JsonProperty("surname")
     private final String surname;
 
-    @NotNull
-    @Pattern(regexp = "[a-zA-Z]{2}\\d{6}[a-dA-D]")
     @JsonProperty("nino")
     private final String nino;
 
-    @NotNull
-    @Past
     @JsonProperty("dateOfBirth")
     private final LocalDate dateOfBirth;
 
-    @NotNull
-    @Valid
     @JsonProperty("address")
     private final AddressDTO address;
 }
