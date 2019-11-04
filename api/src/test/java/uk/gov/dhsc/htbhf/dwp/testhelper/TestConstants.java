d3d693ae748f8d12914c0fcd024721ae3a82dc85
Mike Smith <mike.smith@sescon.co.uk>
2019-11-04 13:33:39 +0000

    HTBHF-2474 Created v2 request objects and tested validation.

--- api/src/test/java/uk/gov/dhsc/htbhf/dwp/testhelper/TestConstants.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/dwp/testhelper/TestConstants.java
@@ -7,6 +7,7 @@ public final class TestConstants {
 
     public static final String HOMER_NINO = "EB123456C";
     public static final String MARGE_NINO = "EB123456D";
+    public static final String VALID_NINO_V2 = "MC123456D";
     public static final String LISA_FORENAME = "Lisa";
     public static final String HOMER_FORENAME = "Homer";
     public static final String MARGE_FORENAME = "Marge";
@@ -18,6 +19,8 @@ public final class TestConstants {
     public static final LocalDate MAGGIE_DOB = LocalDate.now().minusMonths(6);
     public static final LocalDate LISA_DOB = LocalDate.now().minusMonths(24);
     public static final LocalDate BART_DOB = LocalDate.now().minusMonths(48);
+    public static final String HOMER_EMAIL = "homer@simpson.com";
+    public static final String HOMER_MOBILE = "+447700900000";
 
     public static final String SIMPSONS_ADDRESS_LINE_1 = "742 Evergreen Terrace";
     public static final String SIMPSONS_ADDRESS_LINE_2 = "Mystery Spot";
@@ -27,6 +30,7 @@ public final class TestConstants {
     public static final LocalDate ELIGIBLE_END_DATE = LocalDate.parse("2019-03-01");
     public static final LocalDate ELIGIBLE_START_DATE = LocalDate.parse("2019-02-14");
     public static final BigDecimal UC_MONTHLY_INCOME_THRESHOLD = BigDecimal.valueOf(408);
+    public static final int UC_MONTHLY_INCOME_THRESHOLD_IN_PENCE = 40800;
     public static final String SIMPSON_UC_HOUSEHOLD_IDENTIFIER = "ucHouseholdIdentifier";
     public static final String SIMPSON_LEGACY_HOUSEHOLD_IDENTIFIER = "legacyHouseholdIdentifier";
 }
